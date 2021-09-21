6c59ac46fb7d6c2be648480922c7121c92babc36
Gusta Gabriel  de O.L <85789804+gustavo-gabriel-OL@users.noreply.github.com>
2021-09-21 14:58:28 +0000

    2

--- sw.js
+++ sw.js
@@ -4,17 +4,17 @@ var filesToCache = [
     './index.html',
     './manifest.json'
 ];
-self.addEventListener('install', function(e) {
-  e.waitUntil(
+self.addEventListener('install', function(ef) {
+  ef.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(ed) {
+  ed.respondWith(
+    caches.match(ed.request).then(function(response) {
+      return response || fetch(ed.request);
     })
   );
 });
